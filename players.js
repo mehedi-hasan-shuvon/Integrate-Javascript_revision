9f910fb0cae693fdb592902031621c66d1e457da
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-02-18 12:23:49 +0600

    done with event bubble

--- players.js
+++ players.js
@@ -1,20 +1,42 @@
-const players = document.getElementsByClassName('player');
-
-for (const player of players) {
-
-    console.log(player);
-
+function setPlayerStyle(player) {
     player.style.border = "1px solid red";
     player.style.borerRadius = '5px';
     player.style.margin = '10px';
     player.style.padding = '10px';
 
+}
+
+const players = document.getElementsByClassName('player');
+
+for (const player of players) {
+
+    setPlayerStyle(player);
 
 
 }
 
 
-function addPlayer(){
-    const playersContainer=document.getElementById('players');
-    // const player=
+function addPlayer() {
+    const playersContainer = document.getElementById('players');
+    const player = document.createElement('div');
+    player.classList.add('player');
+    player.innerHTML =
+        ` <h4 class="player-name">New Player</h4>
+    <p>Iusto reiciendis, porro consequuntur beatae autem ullam labore maxime eum nesciunt quisquam error dolorum
+        saepe asperiores culpa magni, distinctio tempore ipsum maiores fugit officiis consectetur tempora minima
+        suscipit sit. Ab.</p>
+    `;
+    setPlayerStyle(player);
+    playersContainer.appendChild(player);
+
+    console.log("dsd");
 }
+
+document.getElementById('players').addEventListener('click', function (event) {
+    if (event.target.tagName.toLowerCase() == 'div') {
+        event.target.style.backgroundColor = 'yellow';
+
+    } else {
+        event.target.parentNode.style.backgroundColor = 'blue';
+    }
+})
